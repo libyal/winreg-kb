903ec22df7c5bce8b7cace0770d466d4511f76ff
Joachim Metz <joachim.metz@gmail.com>
2017-02-22 23:16:32 +0100

    Worked on tests

--- tests/knownfolders.py
+++ tests/knownfolders.py
@@ -4,6 +4,11 @@
 
 import unittest
 
+from dfdatetime import filetime as dfdatetime_filetime
+from dfwinreg import definitions as dfwinreg_definitions
+from dfwinreg import fake as dfwinreg_fake
+from dfwinreg import registry as dfwinreg_registry
+
 from winregrc import collector
 from winregrc import knownfolders
 from winregrc import output_writer
@@ -35,24 +40,66 @@ class TestOutputWriter(output_writer.StdoutOutputWriter):
 class KnownFoldersCollectorTest(shared_test_lib.BaseTestCase):
   """Tests for the Windows known folders collector."""
 
-  @shared_test_lib.skipUnlessHasTestFile([u'SOFTWARE'])
+  _GUID = u'374de290-123f-4565-9164-39c4925e467b'
+  _LOCALIZED_NAME = u'@%SystemRoot%\system32\shell32.dll,-21798'
+  _NAME = u'Downloads'
+
+  def _CreateTestRegistry(self):
+    """Creates Registry keys and values for testing.
+
+    Returns:
+      dfwinreg.WinRegistry: Windows Registry for testing.
+    """
+    key_path_prefix = u'HKEY_LOCAL_MACHINE\\Software'
+
+    registry_file = dfwinreg_fake.FakeWinRegistryFile(
+        key_path_prefix=key_path_prefix)
+
+    registry_key = dfwinreg_fake.FakeWinRegistryKey(u'FolderDescriptions')
+    registry_file.AddKeyByPath(
+        u'\\Microsoft\\Windows\\CurrentVersion\\Explorer', registry_key)
+
+    subkey = dfwinreg_fake.FakeWinRegistryKey(self._GUID)
+    registry_key.AddSubkey(subkey)
+
+    value_data = self._NAME.encode(u'utf-16-le')
+    registry_value = dfwinreg_fake.FakeWinRegistryValue(
+        u'Name', data=value_data, data_type=dfwinreg_definitions.REG_SZ)
+    subkey.AddValue(registry_value)
+
+    value_data = self._LOCALIZED_NAME.encode(u'utf-16-le')
+    registry_value = dfwinreg_fake.FakeWinRegistryValue(
+        u'LocalizedName', data=value_data,
+        data_type=dfwinreg_definitions.REG_SZ)
+    subkey.AddValue(registry_value)
+
+    registry_file.Open(None)
+
+    registry = dfwinreg_registry.WinRegistry()
+    registry.MapFile(key_path_prefix, registry_file)
+    return registry
+
   def testCollect(self):
     """Tests the Collect function."""
-    registry_collector = collector.WindowsRegistryCollector()
-
-    test_path = self._GetTestFilePath([u'SOFTWARE'])
-    registry_collector.ScanForWindowsVolume(test_path)
-
-    self.assertIsNotNone(registry_collector.registry)
+    registry = self._CreateTestRegistry()
 
     collector_object = knownfolders.KnownFoldersCollector()
     output_writer = TestOutputWriter()
 
-    collector_object.Collect(registry_collector.registry, output_writer)
-
-    self.assertNotEqual(output_writer.known_folders, [])
+    collector_object.Collect(registry, output_writer)
 
     output_writer.Close()
+
+    self.assertEqual(len(output_writer.known_folders), 1)
+
+    known_folder = output_writer.known_folders[0]
+
+    self.assertIsNotNone(known_folder)
+    self.assertEqual(known_folder.guid, self._GUID)
+    self.assertEqual(known_folder.name, self._NAME)
+    self.assertEqual(
+        known_folder.localized_name,
+        u'@%SystemRoot%\system32\shell32.dll,-21798')
 
 
 if __name__ == '__main__':
