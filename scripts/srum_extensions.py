5f41e1d8d7e09beda36e3e401d6bed0fc84c691b
Joachim Metz <joachim.metz@gmail.com>
2018-01-15 20:38:41 +0100

    Worked on SRUM extensions script

---
+++ scripts/srum_extensions.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Script to extract System Resource Usage Monitor (SRUM) extension information."""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import sys
+
+from winregrc import collector
+from winregrc import output_writer
+from winregrc import srum_extensions
+
+
+class StdoutWriter(output_writer.StdoutOutputWriter):
+  """Stdout output writer."""
+
+  def WriteSRUMExtension(self, srum_extension):
+    """Writes a SRUM extension to the output.
+
+    Args:
+      srum_extension (SRUMExtension): SRUM extension.
+    """
+    text = '{0:s}\t{1:s}'.format(srum_extension.guid, srum_extension.dll_name)
+    self.WriteText(text)
+
+
+def Main():
+  """The main program function.
+
+  Returns:
+    bool: True if successful or False if not.
+  """
+  argument_parser = argparse.ArgumentParser(description=(
+      'Extracts the User Assist information from a NTUSER.DAT Registry file.'))
+
+  argument_parser.add_argument(
+      '--codepage', dest='codepage', action='store', metavar='CODEPAGE',
+      default='cp1252', help='the codepage of the extended ASCII strings.')
+
+  argument_parser.add_argument(
+      '-d', '--debug', dest='debug', action='store_true', default=False,
+      help='enable debug output.')
+
+  argument_parser.add_argument(
+      'source', nargs='?', action='store', metavar='PATH', default=None,
+      help=(
+          'path of the volume containing C:\\Windows, the filename of '
+          'a storage media image containing the C:\\Windows directory,'
+          'or the path of a NTUSER.DAT Registry file.'))
+
+  options = argument_parser.parse_args()
+
+  if not options.source:
+    print('Source value is missing.')
+    print('')
+    argument_parser.print_help()
+    print('')
+    return False
+
+  logging.basicConfig(
+      level=logging.INFO, format='[%(levelname)s] %(message)s')
+
+  output_writer_object = StdoutWriter()
+
+  if not output_writer_object.Open():
+    print('Unable to open output writer.')
+    print('')
+    return False
+
+  registry_collector = collector.WindowsRegistryCollector()
+  if not registry_collector.ScanForWindowsVolume(options.source):
+    print('Unable to retrieve the Windows Registry from: {0:s}.'.format(
+        options.source))
+    print('')
+    return False
+
+  # TODO: map collector to available Registry keys.
+  collector_object = srum_extensions.SRUMExtensionsCollector(
+      debug=options.debug)
+
+  result = collector_object.Collect(
+      registry_collector.registry, output_writer_object)
+  if not result:
+    print('No SRUM extensions key found.')
+
+  output_writer_object.Close()
+
+  return True
+
+
+if __name__ == '__main__':
+  if not Main():
+    sys.exit(1)
+  else:
+    sys.exit(0)
---
+++ winregrc/srum_extensions.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""System Resource Usage Monitor (SRUM) extension collector."""
+
+from __future__ import unicode_literals
+
+from winregrc import interface
+
+
+class SRUMExtension(object):
+  """System Resource Usage Monitor (SRUM) extension.
+
+  Attributes:
+    dll_name (str): DLL name.
+    guid (str): identifier.
+  """
+
+  def __init__(self, guid, dll_name):
+    """Initializes a System Resource Usage Monitor (SRUM) extension.
+
+    Args:
+      guid (str): identifier.
+      dll_name (str): DLL name.
+    """
+    super(SRUMExtension, self).__init__()
+    self.guid = guid
+    self.dll_name = dll_name
+
+
+class SRUMExtensionsCollector(interface.WindowsRegistryKeyCollector):
+  """Windows System Resource Usage Monitor (SRUM) extension collector."""
+
+  _SRUM_EXTENSIONS_KEY_PATH = (
+      'HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows NT\\CurrentVersion\\'
+      'SRUM\\Extensions')
+
+  def Collect(self, registry, output_writer):
+    """Collects the SRUM extensions.
+
+    Args:
+      registry (dfwinreg.WinRegistry): Windows Registry.
+      output_writer (OutputWriter): output writer.
+
+    Returns:
+      bool: True if the SRUM extensions key was found, False if not.
+    """
+    srum_extensions_key = registry.GetKeyByPath(self._SRUM_EXTENSIONS_KEY_PATH)
+    if not srum_extensions_key:
+      return False
+
+    for subkey in srum_extensions_key.GetSubkeys():
+      guid = subkey.name.upper()
+      dll_name = self._GetValueAsStringFromKey(subkey, 'DllName')
+
+      srum_extension = SRUMExtension(guid, dll_name)
+      output_writer.WriteSRUMExtension(srum_extension)
+
+    return True
