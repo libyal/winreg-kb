c01460f684418e8e0b5d4ee35911c82bf1bd161f
Joachim Metz <joachim.metz@gmail.com>
2019-03-02 16:16:15 +0100

    Applied updates

--- config/travis/runtests.sh
+++ config/travis/runtests.sh
@@ -42,13 +42,30 @@ elif test -n "${FEDORA_VERSION}";
 then
 	CONTAINER_NAME="fedora${FEDORA_VERSION}";
 
-	docker exec "${CONTAINER_NAME}" sh -c "git clone https://github.com/libyal/winreg-kb.git";
-
-	if test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	if test -n "${TOXENV}";
 	then
-		docker exec "${CONTAINER_NAME}" sh -c "cd winreg-kb && python2 run_tests.py";
+		docker exec ${CONTAINER_NAME} sh -c "cd winreg-kb && tox -e ${TOXENV}";
+
+	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	then
+		docker exec ${CONTAINER_NAME} sh -c "export LANG=en_US.UTF-8; cd winreg-kb && python2 run_tests.py";
 	else
-		docker exec "${CONTAINER_NAME}" sh -c "cd winreg-kb && python3 run_tests.py";
+		docker exec ${CONTAINER_NAME} sh -c "cd winreg-kb && python3 run_tests.py";
+	fi
+
+elif test -n "${UBUNTU_VERSION}";
+then
+	CONTAINER_NAME="ubuntu${UBUNTU_VERSION}";
+
+	if test -n "${TOXENV}";
+	then
+		docker exec ${CONTAINER_NAME} sh -c "cd winreg-kb && tox -e ${TOXENV}";
+
+	elif test ${TRAVIS_PYTHON_VERSION} = "2.7";
+	then
+		docker exec ${CONTAINER_NAME} sh -c "export LANG=en_US.UTF-8; cd winreg-kb && python2 run_tests.py";
+	else
+		docker exec ${CONTAINER_NAME} sh -c "cd winreg-kb && python3 run_tests.py";
 	fi
 
 elif test "${TRAVIS_OS_NAME}" = "linux";
