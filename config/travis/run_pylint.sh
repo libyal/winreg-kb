761652b1ae5d2408ed76c73cee4ecb7e68da7d74
Joachim Metz <joachim.metz@gmail.com>
2019-12-14 13:18:22 +0100

    Applied updates

--- config/travis/run_pylint.sh
+++ config/travis/run_pylint.sh
@@ -10,7 +10,7 @@ set -e;
 
 pylint --version
 
-for FILE in `find setup.py config scripts tests winregrc -name \*.py`;
+for FILE in $(find setup.py config scripts tests winregrc -name \*.py);
 do
 	echo "Checking: ${FILE}";
 
