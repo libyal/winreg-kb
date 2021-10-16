331cfa78aac5b98eccce6bc44d45be287bb5726c
Joachim Metz <joachim.metz@gmail.com>
2021-10-16 09:41:07 +0200

    Worked on EventLog providers script

--- winregrc/eventlog_providers.py
+++ winregrc/eventlog_providers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Windows EventLog providers collector."""
 
+import logging
+
 from winregrc import interface
 
 
@@ -69,6 +71,8 @@ class EventLogProvidersCollector(interface.WindowsRegistryKeyCollector):
     if not sevices_eventlog_key and not winevt_publishers_key:
       return False
 
+    eventlog_providers = {}
+
     if sevices_eventlog_key:
       for log_type_key in sevices_eventlog_key.GetSubkeys():
         for provider_key in log_type_key.GetSubkeys():
@@ -77,20 +81,29 @@ class EventLogProvidersCollector(interface.WindowsRegistryKeyCollector):
 
           category_message_files = self._GetValueAsStringFromKey(
               provider_key, 'CategoryMessageFile', default_value='')
-          category_message_files = category_message_files.split(';')
+          category_message_files = list(filter(None, [
+              path.strip() for path in category_message_files.split(';')]))
 
           event_message_files = self._GetValueAsStringFromKey(
               provider_key, 'EventMessageFile', default_value='')
-          event_message_files = event_message_files.split(';')
+          event_message_files = list(filter(None, [
+              path.strip() for path in event_message_files.split(';')]))
 
           parameter_message_files = self._GetValueAsStringFromKey(
               provider_key, 'ParameterMessageFile', default_value='')
-          parameter_message_files = parameter_message_files.split(';')
+          parameter_message_files = list(filter(None, [
+              path.strip() for path in parameter_message_files.split(';')]))
 
           eventlog_provider = EventLogProvider(
               category_message_files, event_message_files, None,
               log_source, log_type, parameter_message_files)
-          output_writer.WriteEventLogProvider(eventlog_provider)
+
+          if log_source not in eventlog_providers:
+            eventlog_providers[log_source] = eventlog_provider
+          else:
+            logging.warning((
+                'Found multiple definitions for EventLog provider: '
+                '{0:s}').format(log_source))
 
     if winevt_publishers_key:
       for guid_key in winevt_publishers_key.GetSubkeys():
@@ -98,12 +111,26 @@ class EventLogProvidersCollector(interface.WindowsRegistryKeyCollector):
 
         event_message_files = self._GetValueAsStringFromKey(
             guid_key, 'MessageFileName', default_value='')
-        event_message_files = event_message_files.split(';')
+        event_message_files = list(filter(None, [
+            path.strip() for path in event_message_files.split(';')]))
 
         provider_identifier = guid_key.name.lower()
 
-        eventlog_provider = EventLogProvider(
-            [], event_message_files, provider_identifier, log_source, '', [])
+        existing_eventlog_provider = eventlog_providers.get(log_source, None)
+        if not existing_eventlog_provider:
+          eventlog_provider = EventLogProvider(
+              [], event_message_files, provider_identifier, log_source, '', [])
+
+          eventlog_providers[log_source] = eventlog_provider
+        else:
+          if not existing_eventlog_provider.event_message_files:
+            existing_eventlog_provider.event_message_files = event_message_files
+          if not existing_eventlog_provider.identifier:
+            existing_eventlog_provider.identifier = provider_identifier
+
+          # TODO: compare values if set
+
+    for eventlog_provider in eventlog_providers.values():
         output_writer.WriteEventLogProvider(eventlog_provider)
 
     return True
