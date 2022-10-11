c3976d82f8aa2cb20fce1979c814b0d96a35d6e3
Joachim Metz <joachim.metz@gmail.com>
2022-10-11 06:08:14 +0200

    Applied updates

--- setup.py
+++ setup.py
@@ -28,10 +28,9 @@ except ImportError:
   from distutils.command.sdist import sdist
 
 version_tuple = (sys.version_info[0], sys.version_info[1])
-if version_tuple < (3, 6):
-  print((
-      'Unsupported Python version: {0:s}, version 3.6 or higher '
-      'required.').format(sys.version))
+if version_tuple < (3, 7):
+  print(f'Unsupported Python version: {sys.version:s}, version 3.7 or higher '
+        f'required.')
   sys.exit(1)
 
 # Change PYTHONPATH to include winregrc so that we can get the version.
@@ -88,8 +87,8 @@ else:
           summary = line[9:]
 
         elif line.startswith('BuildRequires: '):
-          line = 'BuildRequires: {0:s}-setuptools, {0:s}-devel'.format(
-              python_package)
+          line = (f'BuildRequires: {python_package:s}-setuptools, '
+                  f'{python_package:s}-devel')
 
         elif line.startswith('Requires: '):
           requires = line[10:]
@@ -112,7 +111,7 @@ else:
 
         elif line.startswith('%files'):
           lines = [
-              '%files -n {0:s}-%{{name}}'.format(python_package),
+              f'%files -n {python_package:s}-%{{name}}',
               '%defattr(644,root,root,755)',
               '%license LICENSE',
               '%doc ACKNOWLEDGEMENTS AUTHORS README']
@@ -132,23 +131,21 @@ else:
         elif line.startswith('%prep'):
           in_description = False
 
-          python_spec_file.append(
-              '%package -n {0:s}-%{{name}}'.format(python_package))
-          python_summary = 'Python 3 module of {0:s}'.format(summary)
+          python_spec_file.append(f'%package -n {python_package:s}-%{{name}}')
+          python_summary = f'Python 3 module of {summary:s}'
 
           python_spec_file.extend([
-              'Requires: {0:s}'.format(requires),
-              'Summary: {0:s}'.format(python_summary),
+              f'Requires: {requires:s}',
+              f'Summary: {python_summary:s}',
               '',
-              '%description -n {0:s}-%{{name}}'.format(python_package)])
+              f'%description -n {python_package:s}-%{{name}}'])
 
           python_spec_file.extend(description)
 
           python_spec_file.extend([
               '%package -n %{name}-tools',
-              'Requires: {0:s}-winreg-kb >= %{{version}}'.format(
-                  python_package),
-              'Summary: Tools for {0:s}'.format(summary),
+              f'Requires: {python_package:s}-winreg-kb >= %{{version}}',
+              f'Summary: Tools for {summary:s}',
               '',
               '%description -n %{name}-tools'])
 
@@ -204,6 +201,12 @@ winregrc_long_description = (
     'winregrc is a Python module part of winreg-kb to allow reuse of Windows '
     'Registry resources.')
 
+command_classes = {'sdist_test_data': sdist}
+if BdistMSICommand:
+  command_classes['bdist_msi'] = BdistMSICommand
+if BdistRPMCommand:
+  command_classes['bdist_rpm'] = BdistRPMCommand
+
 setup(
     name='winregrc',
     version=winregrc.__version__,
@@ -214,10 +217,7 @@ setup(
     url='https://github.com/libyal/winreg-kb',
     maintainer='Joachim Metz',
     maintainer_email='joachim.metz@gmail.com',
-    cmdclass={
-        'bdist_msi': BdistMSICommand,
-        'bdist_rpm': BdistRPMCommand,
-        'sdist_test_data': sdist},
+    cmdclass=command_classes,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
