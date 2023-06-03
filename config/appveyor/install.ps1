d149aff1b8ff97e1cc8d7416fc583b964bad4ccd
Joachim Metz <joachim.metz@gmail.com>
2023-06-03 07:03:39 +0200

    Updated pylintrc to version 2.17

--- config/appveyor/install.ps1
+++ config/appveyor/install.ps1
@@ -6,7 +6,7 @@ If ($Dependencies.Length -gt 0)
 {
 	$Dependencies = ${Dependencies} -split " "
 
-	$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1 | %{ '$_' }"
+	$Output = Invoke-Expression -Command "git clone https://github.com/log2timeline/l2tdevtools.git ..\l2tdevtools 2>&1" | %{ "$_" }
 	Write-Host (${Output} | Out-String)
 
 	If ($env:APPVEYOR_REPO_BRANCH -eq "main")
@@ -21,7 +21,7 @@ If ($Dependencies.Length -gt 0)
 
 	$env:PYTHONPATH = "..\l2tdevtools"
 
-	$Output = Invoke-Expression -Command "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1"
+	$Output = Invoke-Expression -Command "& '${env:PYTHON}\python.exe' ..\l2tdevtools\tools\update.py --download-directory dependencies --machine-type ${env:MACHINE_TYPE} --msi-targetdir ${env:PYTHON} --track ${env:L2TBINARIES_TRACK} ${Dependencies} 2>&1" | %{ "$_" }
 	Write-Host (${Output} | Out-String)
 }
 
